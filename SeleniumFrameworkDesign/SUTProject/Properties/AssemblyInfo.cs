f4775a5282bcd80d25969402f4fed60540fb6ed5
Thejaswi1691 <thejaswi.k@softvision.com>
2020-08-04 18:40:21 +0530

    Initial code Commit

---
+++ SeleniumFrameworkDesign/SUTProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SUTProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SUTProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9a604d6-e2b7-4c04-87eb-e6eb7863c71a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumFrameworkDesign/SUTProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        String URL = "http://eaapp.somee.com/";
+        private IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(URL);
+            Login();
+        }
+        public void Login()
+        {
+            driver.FindElement(By.Id("loginLink")).Click();
+            driver.FindElement(By.Id("UserName")).SendKeys("thejaswi");
+            IWebElement login = driver.FindElement(By.Id("Password"));
+            login.SendKeys("Sweety.1234");
+            login.SendKeys(Keys.Enter);
+            driver.Close();
+        }
+
+    }
+}
